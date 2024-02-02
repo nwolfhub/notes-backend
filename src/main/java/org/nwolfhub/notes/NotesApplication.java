d9140b69151c55a5337b1ca2fd5bca15962ba059
s0m31 <admin@nwolfhub.org>
2024-02-02 13:44:36 +0300

    Remove legacy code in NotesApplication

--- src/main/java/org/nwolfhub/notes/NotesApplication.java
+++ src/main/java/org/nwolfhub/notes/NotesApplication.java
@@ -2,10 +2,6 @@ package org.nwolfhub.notes;
 
 import org.nwolfhub.easycli.Defaults;
 import org.nwolfhub.easycli.EasyCLI;
-import org.nwolfhub.notes.api.legacy.NotesController;
-import org.nwolfhub.notes.api.legacy.UserController;
-import org.nwolfhub.notes.database.legacy.TokenController;
-import org.nwolfhub.notes.database.legacy.UserDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -18,9 +14,6 @@ public class NotesApplication {
 		cli.addTemplate(Defaults.defaultTemplate);
 		SpringApplication.run(NotesApplication.class, args);
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Configurator.class);
-		NotesController.init((org.nwolfhub.utils.Configurator) context.getBean("privilegesConfigurator"), context.getBean(UserDao.class));
-		TokenController.init();
-		UserController.init(context.getBean(UserDao.class));
 	}
 
 }
