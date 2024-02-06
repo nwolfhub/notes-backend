8cbf363eca8d6c7a21d54e672e5779cf2a83dc2e
s0m31 <admin@nwolfhub.org>
2024-02-06 12:05:19 +0300

    Minor changes in ServerInfo.class

--- src/main/java/org/nwolfhub/notes/api/ServerInfo.java
+++ src/main/java/org/nwolfhub/notes/api/ServerInfo.java
@@ -1,5 +1,6 @@
 package org.nwolfhub.notes.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/server")
 public class ServerInfo {
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    String issuer;
     @GetMapping("/info")
     public String getServerInfo() {
         return JsonBuilder.serverInfo;
+    }
+    @GetMapping("/login")
+    public String getLoginInfo() {
+        return JsonBuilder.buildIndirectLogin(issuer + "/protocol/openid-connect/auth");
     }
 }
